6a6a0f744778b508c4a78cdd7ecb6528d578551f
lihuixian <qqiang200689@gmail.com>
2017-10-27 22:20:19 +0800

    define a function

--- check_ip.py
+++ check_ip.py
@@ -1,16 +1,21 @@
 #coding=utf8
 import re
-check_IP=raw_input("please input the IP address:")
-num=0
-if not re.search('[a-zA-Z]', check_IP):
-    for i in check_IP.split('.'):
-        if int(i)>=0 and int(i)<256:
-            num+=1
-            if num==4:
-                print 'this ip is ok'
-            continue
-        else:
-            print "this %s is ilegal"
-            break
-else:
-    print "this %s is ilegal"
+def check_IP(IP):
+    num=0
+    if not re.search('[a-zA-Z]', IP):
+        for i in IP.split('.'):
+            if int(i)>=0 and int(i)<256:
+                num+=1
+                if num==4:
+                    print 'this ip is ok'
+                continue
+            else:
+                print "this ip is ilegal"
+                break
+    else:
+        print "this ip is ilegal"
+
+if __name__=='__main__':
+    IP_address = raw_input("please input the IP address:")
+    check_IP(IP_address)
+    #check_IP('1.1.1.1')
